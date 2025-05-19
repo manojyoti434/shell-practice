42739ac4ab9d34420c25d5bc3123cf1f127576b6
mano jyoti <manojyoti434@gmail.com>
2025-05-19 16:35:48 +0530

    variables external

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
 echo "hello world"
+# git usage
+#u need to constaruct code/fie in visual studio
+#push that into repos using local gitbash-->gets pushed to repos in github main
+#pull the updated git into linux instance using git pull then you can execute script shell in instance
---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1 #do not put spaces before or after equal to#
+person2=$2
+
+echo "$person1: Hey $person2 how are you doing?"
+echo "$person2: hi $person1, i am good, how are you?"
+echo "$person1: i am good and i am going to India, will you come?"
+echo "$person2: i am fine here only, you carry on"
