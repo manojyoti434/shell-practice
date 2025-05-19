a74cf5fd122a4d2ac0fe2702d203e96b477556e4
mano jyoti <manojyoti434@gmail.com>
2025-05-19 16:39:57 +0530

    variables external

---
+++ 05-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "enter your pin number"
+read -s PIN #reads pin number with hiding by using s and PIN is variable #
+# pin number gets read and appende to PIN variable#
+echo "your pin number is: $PIN"
