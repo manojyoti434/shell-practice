c5562ea9837a3d8b40039a968190d53a62835ca7
mano jyoti <manojyoti434@gmail.com>
2025-05-19 21:37:20 +0530

    variables special

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -5,7 +5,7 @@ echo "number of variable spassed: $#"
 echo "script name: $0"
 echo "present working directory: $PWD"
 
-echo "home directory of user who is running script: $home"
+echo "home directory of user who is running script: $HOME"
 echo "user who is running the script: $user"
 echo "pid of current running script : $$"
 sleep 10 &
