2892936fe7aecff1469867378d0268533dc5d496
mano jyoti <manojyoti434@gmail.com>
2025-05-19 16:56:01 +0530

    variables external

--- 07-array.sh
+++ 07-array.sh
@@ -2,5 +2,8 @@
 
 MOVIES=("court" "hit3" "pushpa2" "singles")
 
-echo "First movie: ${MOVIES[1]}"
+echo "First movie: ${MOVIES[0]}"
+echo "second movie: ${MOVIES[1]}"
+echo all movies are ${MOVIES[@]}"
+
 
