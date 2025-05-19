66a35ebf7daec01b7f6407ed04b7339c20ac1232
mano jyoti <manojyoti434@gmail.com>
2025-05-19 21:13:22 +0530

    variables external

---
+++ 08-special variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+number of variable spassed: $#
+script name: $0
+present working directory: $PWD
+all variables passed to script: $@
+home directory of user who is running script: $home
+user who is running the script: $user
+pid of current running script : $$
+sleep 10 &
+PID of last command running in background: $!
+
