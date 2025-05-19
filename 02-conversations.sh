32ef286b22b30c929e3faee0af9102abae70d831
mano jyoti <manojyoti434@gmail.com>
2025-05-19 16:23:46 +0530

    modified

--- 02-conversations.sh
+++ 02-conversations.sh
@@ -4,5 +4,5 @@
 
 echo "trump: Hey Musk how are you doing?"
 echo "musk: hi Trump, i am good, how are you?"
-echo "trump: i am good and i am going to India, will you come?
+echo "trump: i am good and i am going to India, will you come?"
 echo "musk: i am fine here only, you carry on"
--- 03-variables.sh
+++ 03-variables.sh
@@ -5,5 +5,5 @@ person2=musk
 
 echo "$person1: Hey $person2 how are you doing?"
 echo "$person2: hi $person1, i am good, how are you?"
-echo "$person1: i am good and i am going to India, will you come?
+echo "$person1: i am good and i am going to India, will you come?"
 echo "$person2: i am fine here only, you carry on"
