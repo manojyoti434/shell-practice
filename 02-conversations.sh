3f17f626f43147df9f83a6ea17d8eded0677c903
mano jyoti <manojyoti434@gmail.com>
2025-05-19 16:03:20 +0530

    variables and converasations

---
+++ 02-conversations.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+
+echo "trump: Hey Musk how are you doing?"
+echo "musk: hi Trump, i am good, how are you?"
+echo "trump: i am good and i am going to India, will you come?
+echo musk: i am fine here only, you carry on"
---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=trump
+person2=musk
+
+echo "$person1: Hey $person2 how are you doing?"
+echo "$person2: hi $person1, i am good, how are you?"
+echo "$person1: i am good and i am going to India, will you come?
+echo $person2: i am fine here only, you carry on"
