3a81cba4ef9a70f67907050991773190b341c8aa
mano jyoti <manojyoti434@gmail.com>
2025-05-19 22:27:58 +0530

    variables special

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -11,7 +11,9 @@ else
 fi
 
 dnf list installed mysql
-
+#checks already installed or not if installed$? is 0,then expression is false and go to else condition
+#if not installed already then $? is not 0, exp is 1 ne 0 becomes true and installs the mysql
+#post that checks the proper installed or not in seconf if condition#
 if [ $? -ne 0 ]
 then
     echo "MYSQL is not installed ... going to install it"
