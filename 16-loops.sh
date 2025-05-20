3bb36aeb27c91e2326e60c8e91abaed867e3f2ef
mano jyoti <manojyoti434@gmail.com>
2025-05-20 19:35:15 +0530

    loops

--- 16-loops.sh
+++ 16-loops.sh
@@ -37,7 +37,8 @@ VALIDATE(){
 }
 
 
-for PACKAGE in ${PACKAGES[@]}
+#for PACKAGE in ${PACKAGES[@]}
+for PACKAGE in $@
     do
         dnf list installed $PACKAGE &>>LOG_FILE
         if [ $? -ne 0 ]
